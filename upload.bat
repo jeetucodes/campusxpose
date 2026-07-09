88718f6ac8f00af45781c2aa5bdb280a100882f1
jeetucodes <jaishriram5400@gmail.com>
2026-07-09 19:51:13 +0530

    share btn update

--- upload.bat
+++ upload.bat
@@ -3,6 +3,16 @@ echo =========================================
 echo      CampusXpose Code Uploader
 echo =========================================
 
+echo.
+echo Configuring Git credentials...
+git config user.name "jeetucodes"
+git config user.email "jaishriram5400@gmail.com"
+
+echo Configuring remote repository...
+git remote remove origin 2>nul
+git remote add origin https://github.com/jeetucodes/campusxpose.git
+git branch -M main
+
 set /p msg="Enter commit message (or press enter for default 'Update'): "
 if "%msg%"=="" set msg=Update
 
@@ -16,7 +26,7 @@ git commit -m "%msg%"
 
 echo.
 echo [3/3] Pushing to repository...
-git push
+git push -u origin main
 
 echo.
 echo =========================================
